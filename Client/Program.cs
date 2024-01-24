96a831054bc3e9b92eaddcd5e3430db13a0e93e3
shaai22 <shaai.22@uni-dubna.ru>
2024-01-24 20:28:25 +0300

    Добавьте файлы проекта.

---
+++ Client/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Клиент запущен");
+            NamedPipeClientStream npcs = new NamedPipeClientStream("PipeDream");
+            Console.WriteLine("Подключаюсь к серверу...");
+            npcs.Connect();
+            Console.WriteLine("Подключен!");
+            StreamWriter sw = new StreamWriter(npcs);
+            StreamReader sr = new StreamReader(npcs);
+            sw.AutoFlush = true;
+
+            while (true)
+            {
+                Console.Write("> ");
+                sw.WriteLine(Console.ReadLine());
+                Console.WriteLine($"< {sr.ReadLine()}");
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading;
+
+namespace Server
+{
+    internal class Program
+    {
+        public static int tr = 0;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сервер запущен");
+            Thread firstThread = new Thread(client);
+            firstThread.Start();
+        }
+
+        public static void client()
+        {
+            tr++;
+            Console.WriteLine($"Поток {tr}");
+            NamedPipeServerStream npss = new NamedPipeServerStream("PipeDream", PipeDirection.InOut, 10);
+            Console.WriteLine("Ожидаю подключение...");
+            npss.WaitForConnection();
+
+            Thread nextThread = new Thread(client);
+            nextThread.Start();
+
+            Console.WriteLine("Клиент подключен");
+            StreamReader sr = new StreamReader(npss);
+            StreamWriter sw = new StreamWriter(npss);
+            sw.AutoFlush = true;
+
+            while (true)
+            {
+                string message = sr.ReadLine();
+                Console.WriteLine($"{tr-1}> {message}");
+                sw.WriteLine($"Клиент: {message}");
+            }
+        }
+    }
+}
